43ff3cc14b6d8c723e7713b00c2d7ada05e91584
Juan Ignacio Folco <62778900+juanfolco@users.noreply.github.com>
2022-07-11 12:30:00 -0300

    Removed Jackhmmer pairing method
    It wasn't being used

--- alphafoldxplore.py
+++ alphafoldxplore.py
@@ -150,117 +150,6 @@ def set_up():
 
   from alphafold.common import protein
 
-def run_jackhmmer(sequence, prefix):
-
-    fasta_path = f"{prefix}.fasta"
-    with open(fasta_path, 'wt') as f:
-      f.write(f'>query\n{sequence}')
-
-    pickled_msa_path = f"{prefix}.jackhmmer.pickle"
-    if os.path.isfile(pickled_msa_path):
-      msas_dict = pickle.load(open(pickled_msa_path,"rb"))
-      msas, deletion_matrices, names = (msas_dict[k] for k in ['msas', 'deletion_matrices', 'names'])
-      full_msa = []
-      for msa in msas:
-        full_msa += msa
-    else:
-      # --- Find the closest source ---
-      test_url_pattern = 'https://storage.googleapis.com/alphafold-colab{:s}/latest/uniref90_2021_03.fasta.1'
-      ex = futures.ThreadPoolExecutor(3)
-      def fetch(source):
-        request.urlretrieve(test_url_pattern.format(source))
-        return source
-      fs = [ex.submit(fetch, source) for source in ['', '-europe', '-asia']]
-      source = None
-      for f in futures.as_completed(fs):
-        source = f.result()
-        ex.shutdown()
-        break
-
-      jackhmmer_binary_path = '/usr/bin/jackhmmer'
-      dbs = []
-
-      num_jackhmmer_chunks = {'uniref90': 59, 'smallbfd': 17, 'mgnify': 71}
-      total_jackhmmer_chunks = sum(num_jackhmmer_chunks.values())
-      with tqdm.notebook.tqdm(total=total_jackhmmer_chunks, bar_format=TQDM_BAR_FORMAT) as pbar:
-        def jackhmmer_chunk_callback(i):
-          pbar.update(n=1)
-
-        pbar.set_description('Searching uniref90')
-        jackhmmer_uniref90_runner = jackhmmer.Jackhmmer(
-            binary_path=jackhmmer_binary_path,
-            database_path=f'https://storage.googleapis.com/alphafold-colab{source}/latest/uniref90_2021_03.fasta',
-            get_tblout=True,
-            num_streamed_chunks=num_jackhmmer_chunks['uniref90'],
-            streaming_callback=jackhmmer_chunk_callback,
-            z_value=135301051)
-        dbs.append(('uniref90', jackhmmer_uniref90_runner.query(fasta_path)))
-
-        pbar.set_description('Searching smallbfd')
-        jackhmmer_smallbfd_runner = jackhmmer.Jackhmmer(
-            binary_path=jackhmmer_binary_path,
-            database_path=f'https://storage.googleapis.com/alphafold-colab{source}/latest/bfd-first_non_consensus_sequences.fasta',
-            get_tblout=True,
-            num_streamed_chunks=num_jackhmmer_chunks['smallbfd'],
-            streaming_callback=jackhmmer_chunk_callback,
-            z_value=65984053)
-        dbs.append(('smallbfd', jackhmmer_smallbfd_runner.query(fasta_path)))
-
-        pbar.set_description('Searching mgnify')
-        jackhmmer_mgnify_runner = jackhmmer.Jackhmmer(
-            binary_path=jackhmmer_binary_path,
-            database_path=f'https://storage.googleapis.com/alphafold-colab{source}/latest/mgy_clusters_2019_05.fasta',
-            get_tblout=True,
-            num_streamed_chunks=num_jackhmmer_chunks['mgnify'],
-            streaming_callback=jackhmmer_chunk_callback,
-            z_value=304820129)
-        dbs.append(('mgnify', jackhmmer_mgnify_runner.query(fasta_path)))
-
-      # --- Extract the MSAs and visualize ---
-      # Extract the MSAs from the Stockholm files.
-      # NB: deduplication happens later in pipeline.make_msa_features.
-
-      mgnify_max_hits = 501
-      msas = []
-      deletion_matrices = []
-      names = []
-      for db_name, db_results in dbs:
-        unsorted_results = []
-        for i, result in enumerate(db_results):
-          msa, deletion_matrix, target_names = parsers.parse_stockholm(result['sto'])
-          e_values_dict = parsers.parse_e_values_from_tblout(result['tbl'])
-          e_values = [e_values_dict[t.split('/')[0]] for t in target_names]
-          zipped_results = zip(msa, deletion_matrix, target_names, e_values)
-          if i != 0:
-            # Only take query from the first chunk
-            zipped_results = [x for x in zipped_results if x[2] != 'query']
-          unsorted_results.extend(zipped_results)
-        sorted_by_evalue = sorted(unsorted_results, key=lambda x: x[3])
-        db_msas, db_deletion_matrices, db_names, _ = zip(*sorted_by_evalue)
-        if db_msas:
-          if db_name == 'mgnify':
-            db_msas = db_msas[:mgnify_max_hits]
-            db_deletion_matrices = db_deletion_matrices[:mgnify_max_hits]
-            db_names = db_names[:mgnify_max_hits]
-          msas.append(db_msas)
-          deletion_matrices.append(db_deletion_matrices)
-          names.append(db_names)
-          msa_size = len(set(db_msas))
-          print(f'{msa_size} Sequences Found in {db_name}')
-
-        pickle.dump({"msas":msas,
-                    "deletion_matrices":deletion_matrices,
-                    "names":names}, open(pickled_msa_path,"wb"))
-  # return msas, deletion_matrices, names
-
-"""## **Creo un diccionario con entrada de archivos multifasta y salida diccionario con keys= jobname y value= sequence**
-
-## **Corro todo el software en un solo bloque que me genera los zip de salida**
-
-Acá puse el diccionario dentro del run principal, hay que hacer la función
-
-TODA FUNCION JUNTO
-"""
 
 def predict (zfile): #se le pasa la dirección a un archivo FASTA
   TQDM_BAR_FORMAT = '{l_bar}{bar}| {n_fmt}/{total_fmt} [elapsed: {elapsed} remaining: {remaining}]'
@@ -440,11 +329,6 @@ def predict (zfile): #se le pasa la dirección a un archivo FASTA
             a3m_lines = A3M_LINES[n]
             msa, mtx = parsers.parse_a3m(a3m_lines)
             msas_, mtxs_ = [msa],[mtx]
-    
-          elif msa_method == "jackhmmer":
-            print(f"running jackhmmer on seq_{n}")
-            # run jackhmmer
-            msas_, mtxs_, names_ = ([sum(x,())] for x in run_jackhmmer(seq, prefix))
         
           # pad sequences
           for msa_,mtx_ in zip(msas_,mtxs_):
@@ -479,13 +363,6 @@ def predict (zfile): #se le pasa la dirección a un archivo FASTA
             _msa, _mtx, _lab = pairmsa.parse_a3m(a3m_lines,
                                               filter_qid=pair_qid/100,
                                               filter_cov=pair_cov/100)
-
-          elif msa_method == "jackhmmer":
-            _msas, _mtxs, _names = run_jackhmmer(_seq, _prefix)
-            _msa, _mtx, _lab = pairmsa.get_uni_jackhmmer(_msas[0], _mtxs[0], _names[0],
-                                                      filter_qid=pair_qid/100,
-                                                      filter_cov=pair_cov/100)
-
           if len(_msa) > 1:
             _data.append(pairmsa.hash_it(_msa, _lab, _mtx, call_uniprot=False))
           else:
